18fc326c89bd8760cb99a91349ec5eea6cb0381a
OuterScripts <157838073+OuterScripts@users.noreply.github.com>
2024-01-31 11:57:26 -0600

    e

--- Installer.lua
+++ Installer.lua
@@ -1,13 +1,17 @@
-local lplr = game:GetService("Players").LocalPlayer
+local lplr = game:GetService('Players').LocalPlayer
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Installer", HidePremium = false, SaveConfig = false, ConfigFolder = "idk", IntroEnabled = false})
-local hwid = game:GetService("RbxAnalyticsService"):GetClientId()
-local playerservice = game:GetService("Players")
-local replicatedstorage = game:GetService("ReplicatedStorage")
-local BedwarsLobby = game:HttpGet("https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039.vapeprofile.txt")
-local BedwarsGUI = game:HttpGet("https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039GUIPositions.vapeprofile.txt")
-local GamePR = game:HttpGet("https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872274481.vapeprofile.txt")
+local Window = OrionLib:MakeWindow({Name = 'Installer', HidePremium = false, SaveConfig = false, ConfigFolder = 'idk', IntroEnabled = false})
+local hwid = game:GetService('RbxAnalyticsService'):GetClientId()
+local playerservice = game:GetService('Players')
+local replicatedstorage = game:GetService('ReplicatedStorage')
+local BedwarsLobby = game:HttpGet('https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039.vapeprofile.txt')
+local BedwarsGUI = game:HttpGet('https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039GUIPositions.vapeprofile.txt')
+local GamePR = game:HttpGet('https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872274481.vapeprofile.txt')
 
+local isfile = isfile or function(file) -- i needed it, ty render
+	local suc, res = pcall(function() return readfile(file) end)
+	return suc and res ~= nil
+end
 
 local Tab = Window:MakeTab({
 	Name = "Installer",
@@ -17,13 +21,13 @@ local Tab = Window:MakeTab({
 
 
 Tab:AddButton({
-	Name = "Install",
+	Name = 'Install',
 	Callback = function()
 		if not isfile('vape/Profiles/6872265039.vapeprofile.txt') then
             writefile('vape/Profiles/6872265039.vapeprofile.txt', '')
         end
 
-        if not isfile("vape/Profiles/6872265039GUIPositions.vapeprofile.txt') then
+        if not isfile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt') then
             writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', '')
         end
 
@@ -33,22 +37,38 @@ Tab:AddButton({
 
         task.wait(0.5)
     
-        delfile('vape/Profiles/6872265039.vapeprofile.txt')
-        delfile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt')
-        delfile('vape/Profiles/6872274481.vapeprofile.txt')
+        if isfile('vape/Profiles/6872265039.vapeprofile.txt') then
+            delfile('vape/Profiles/6872265039.vapeprofile.txt')
+        end
+
+        if isfile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt')
+            delfile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt')
+        end
+
+        if isfile('vape/Profiles/6872274481.vapeprofile.txt')
+            delfile('vape/Profiles/6872274481.vapeprofile.txt')
+        end
       
         task.wait(0.5)
-    
-        writefile('vape/Profiles/6872265039.vapeprofile.txt', BedwarsLobby)
-        writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', BedwarsGUI)
-        writefile("vape/Profiles/6872274481.vapeprofile.txt", GamePR)
+
+        if not isfile('vape/Profiles/6872265039.vapeprofile.txt') then
+            writefile('vape/Profiles/6872265039.vapeprofile.txt', BedwarsLobby)
+        end
+
+        if not isfile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt') then
+            writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', BedwarsGUI)
+        end
+
+        if not isfile('vape/Profiles/6872274481.vapeprofile.txt') then
+            writefile('vape/Profiles/6872274481.vapeprofile.txt', GamePR)
+        end
 		
 		task.wait(1)
 
 		OrionLib:MakeNotification({
-		    Name = "Installer",
-			Content = "Done!",
-		    Image = "rbxassetid://4483345998",
+		    Name = 'Installer',
+			Content = 'Done!',
+		    Image = 'rbxassetid://4483345998',
 		    Time = 5
 		})
   	end    
--- fixedinstaller.lua
+++ fixedinstaller.lua
@@ -1,33 +1,33 @@
 local getservice = game:GetService
-local lplr = getservice("Players").LocalPlayer
+local lplr = getservice('Players').LocalPlayer
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Installer", HidePremium = false, SaveConfig = false, ConfigFolder = "idk", IntroEnabled = false})
-local hwid = getservice("RbxAnalyticsService"):GetClientId()
-local playerservice = getservice("Players")
-local replicatedstorage = shortcuts.getservice("ReplicatedStorage")
-local httpservice = getservice("HttpService")
-local BedwarsLobby = game:HttpGet("https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039.vapeprofile.txt")
-local BedwarsGUI = game:HttpGet("https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039GUIPositions.vapeprofile.txt")
-local GamePR = game:HttpGet("https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872274481.vapeprofile.txt")
+local Window = OrionLib:MakeWindow({Name = 'Installer', HidePremium = false, SaveConfig = false, ConfigFolder = 'idk', IntroEnabled = false})
+local hwid = getservice('RbxAnalyticsService'):GetClientId()
+local playerservice = getservice('Players')
+local replicatedstorage = shortcuts.getservice('ReplicatedStorage')
+local httpservice = getservice('HttpService')
+local BedwarsLobby = game:HttpGet('https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039.vapeprofile.txt')
+local BedwarsGUI = game:HttpGet('https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872265039GUIPositions.vapeprofile.txt')
+local GamePR = game:HttpGet('https://raw.githubusercontent.com/OuterScripts/BedrockWareee/main/6872274481.vapeprofile.txt')
 
 
 OrionLib:MakeNotification({
-    Name = "Installer",
-    Content = "i dont know if this is a fix.",
-    Image = "rbxassetid://4483345998",
+    Name = 'Installer',
+    Content = 'i dont know if this is a fix.',
+    Image = 'rbxassetid://4483345998',
     Time = 5
 })
 
 
 local Tab = Window:MakeTab({
-	Name = "Installer",
-    Icon = "rbxassetid://4483345998",
+	Name = 'Installer',
+    Icon = 'rbxassetid://4483345998',
     PremiumOnly = false
 })
 
 
 Tab:AddButton({
-	Name = "Install",
+	Name = 'Install',
 	Callback = function()
 		delfolder('vape/Profiles')
       
@@ -39,14 +39,14 @@ Tab:AddButton({
     
         writefile('vape/Profiles/6872265039.vapeprofile.txt', BedwarsLobby)
         writefile('vape/Profiles/6872265039GUIPositions.vapeprofile.txt', BedwarsGUI)
-        writefile("vape/Profiles/6872274481.vapeprofile.txt", GamePR)
+        writefile('vape/Profiles/6872274481.vapeprofile.txt', GamePR)
 		
 		task.wait(1)
 
 		OrionLib:MakeNotification({
-		    Name = "Installer",
-			Content = "Done!",
-		    Image = "rbxassetid://4483345998",
+		    Name = 'Installer',
+			Content = 'Done!',
+		    Image = 'rbxassetid://4483345998',
 		    Time = 5
 		})
   	end    
